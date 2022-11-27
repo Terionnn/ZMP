0f1ffa0f62957d55928d62f4b5f211a6255fa14d
Terionnn <kamil142401@gmail.com>
2022-11-27 01:02:46 +0100

    etap3

--- src/main.cpp
+++ src/main.cpp
@@ -94,9 +94,8 @@ int main()
       "AddObj Name=Podstawa1.Ramie1.Ramie2 RGB=(100,200,0) Scale=(2,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(3,0,0)\n"
       "AddObj Name=Podstawa2 RGB=(20,200,10) Scale=(4,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(-1,-3,0)\n"
       "AddObj Name=Podstawa2.Ramie1 RGB=(100,0,0) Scale=(3,3,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(4,0,0)\n"
-      "AddObj Name=Podstawa2.Ramie1.Ramie2 RGB=(100,200,0) Scale=(2,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(3,0,0)\n";
+      "AddObj Name=Podstawa2.Ramie1.Ramie2 RGB=(100,240,0) Scale=(2,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(3,0,0)\n";
 
   sender.Send(sConfigCmds);
   sender.Send("Close\n");
-
 }
